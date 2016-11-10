34ec8520825d6649aa080647d4c413e97f07edc0
Joshua Rieken <joshua@joshuarieken.com>
2016-11-10 15:02:49 -0600

    Use build_con() instead of deprecated conn()

--- lib/views/helpers.ex
+++ lib/views/helpers.ex
@@ -1,7 +1,7 @@
 defmodule ESpec.Phoenix.Views.Helpers do
   defmacro __using__(args) do
     quote do
-      def render_template(template, assigns, conn \\ conn()) do
+      def render_template(template, assigns, conn \\ build_conn()) do
         assigns = Keyword.put_new(assigns, :conn, conn)
         Phoenix.View.render_to_string @view, template, assigns
       end
