3f9932a88568184d3812535d70f0dab8775a69c0
Joshua Rieken <joshua@joshuarieken.com>
2017-10-12 14:07:26 -0600

    Fix warnings

--- lib/views/helpers.ex
+++ lib/views/helpers.ex
@@ -1,5 +1,5 @@
 defmodule ESpec.Phoenix.Views.Helpers do
-  defmacro __using__(args) do
+  defmacro __using__(_args) do
     quote do
       def render_template(template, assigns, conn \\ build_conn()) do
         assigns = Keyword.put_new(assigns, :conn, conn)
